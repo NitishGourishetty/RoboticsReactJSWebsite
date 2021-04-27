4583b1b8c24ad1ab44d63e2d0682b7bec1132722
NitishGourishetty <59469935+NitishGourishetty@users.noreply.github.com>
2021-04-27 16:03:46 -0700

    Update app.js
    Put correct File

--- app.js
+++ app.js
@@ -1,40 +1,76 @@
-const Sequelize = require("sequelize"),
-    express = require("express"),
-    app = express(),
-    bodyParser = require("body-parser")
-//Enter real passwrod in
+//const http = require('http');
+const express = require("express");
+const app = express();
+const Sequelize = require("sequelize");
 const connection = new Sequelize('robotics_schema', 'root', 'password!', {
-    dialect: 'mysql'
-});
-app.use(bodyParser.urlencoded({ extender: true }));
-app.set("view engine", "ejs");
-
-const Article = connection.define('article', {
-    title: Sequelize.STRING,
-    body: Sequelize.STRING
-}, {
-    timestamps: false,
+	dialect: 'mysql'
 });
 
-app.get("/", function (req, res) {
-    res.render("graph_page")
-});
+let dataset = {};
 
-app.listen(3000, function () {
-    console.log("The YelpCamp Server has started");
-});
+// connection
+// 	.sync({
+// 		logging: console.log
+// 	})
+// 	.then(function() {
+// 		Article.findAll().then(function(articles) {
+// 			dataset = articles;
+// 		})
+// 	})
+// 	.catch(function(error) {
+// 		console.log(error);
+// 	});
 
-connection
-    .sync({
-        force: true,
-        logging: console.log
-    })
-    .then(function () {
-        return Article.create({
-            title: 'Graph 1',
-            body: 12
-        });
-    })
-    .catch(function (error) {
-        console.log(error);
+//const server = http.createServer((req, res) => {
+		const Article = connection.define('article', {
+			data: Sequelize.INTEGER,
+			graph: Sequelize.STRING,
+			num1: Sequelize.INTEGER,
+			num2: Sequelize.INTEGER,
+			num3: Sequelize.INTEGER
+
+	}, {
+		timestamps: false
+	});
+
+    //Sending a JSON of the SQL stuff to port 8000, to get read my app.s in my-app later
+	app.get("/", (req, res) => {
+		//res.send(JSON.stringify(['Objects', 'Antoehr', '17']));
+		console.log("Hitting");
+		res.header("Access-Control-Allow-Origin", "*");
+    	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    	res.set("Access-Control-Allow-Origin", "*");
+    	Article.findAll().then(function(articles) {
+			dataset = articles;
+			console.log(dataset);
+			res.json({
+			//parse it here
+        	data: (dataset)
+		})
+		
     });
+	});
+
+	//console.log(Article.findAll());
+
+	
+
+	// if(req.url === '/') {
+	// 	res.write('Hello World');
+	// 	res.end();
+	// }
+
+	// if(req.url === '/hi') {
+	// 	res.write(JSON.stringify([1, 2, 3]));
+	// 	res.end();
+	// }
+//});
+
+//const Article = connection.define('article', )
+
+
+//server.listen(3000);
+
+app.listen(8000,() =>{
+	console.log("The  Server has started");
+});
